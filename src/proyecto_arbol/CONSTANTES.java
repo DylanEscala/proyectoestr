c0e8560abd40870f08859f3d644440157c1b6983
Dylan David Escala Bonilla <descala@espol.edu.ec>
2019-12-29 04:16:26 +0000

    CONSTANTES.java created online with Bitbucket

---
+++ src/proyecto_arbol/CONSTANTES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+
+/**
+ *
+ * @author Alex Velez
+ */
+public final class CONSTANTES {
+    private CONSTANTES(){}
+    public static final double WIDTH =450;
+    public static final double HEIGHT =600;
+    public static final String RUTA_IMGS="/recursos/imagenes/";
+    public static final String RUTA_TEXT="src/recursos/texto/";
+    public static final Font MYFONT = Font.font("Arial", FontWeight.NORMAL, FontPosture.REGULAR, 25);
+}
